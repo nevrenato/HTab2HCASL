81431cca15a9f96bfcbb824b4eb1152c4ef51163
Renato Neves <nevrenato@gmail.com>
2013-06-15 22:29:40 +0100

    Removed the ambiguity problem, by adding parenthesis. Cleaned part of the code.

--- converter.hs
+++ converter.hs
@@ -9,6 +9,9 @@ import HyLo.Signature.String (NomSymbol(..), PropSymbol(..), RelSymbol(..))
 import Data.Set hiding (foldr)
 import Data.List (intersperse)
 
+type SymSet = (Set String, Set String, Set String) 
+triEmpty = (empty, empty, empty)
+
 main :: FilePath -> IO ()
 main f = (convertToCASL f) >>= (writeFile (f++"c"))
 
@@ -21,10 +24,34 @@ parseWithIO s = s >>= (return . parse)
 
 toCASLConverter :: InputFile -> String
 toCASLConverter i = 
-	(addCASLHeader (collectSymbols i)) $ unlines $ (foldr (\a b -> f a : b) [] i)
-	where f _ = ""
------------------------
-type SymSet = (Set String, Set String, Set String) 
+	(addCASLHeader (collectSymbols i)) $ 
+		connectByConj $ (foldr (\a b -> converter a : b) [] i)
+	where 
+		connectByConj = unlines . (intersperse ("/\\"))	
+
+converter :: (Formula NomSymbol PropSymbol RelSymbol) -> String
+converter f = case f of 
+	Top -> "true"
+	Bot -> "false"
+	(Prop p) -> ppp p
+	(Nom n) -> "Here "++(ppn n)
+	(Neg g) -> "not "++(convWrap g)
+	(g :&: g') -> wrap (convWrap g) "/\\" (convWrap g')
+	(g :|: g') -> wrap (convWrap g) "\\/" (convWrap g')
+	(g :-->: g') -> wrap (convWrap g) " => " (convWrap g')
+	(g :<-->: g') -> wrap (convWrap g) "<==>" (convWrap g')
+	(Diam r g) -> wrapInDia (ppm r)++(convWrap g)	
+	(Box r g) -> wrapInBox (ppm r)++(convWrap g)
+	(At n g) -> wrap "@" (ppn n) (convWrap g)
+	--f (A g) s = f g s
+	--f (E g) s = f g s
+	--f (Down n g) s = f g s
+	where
+		--pretty printer
+		ppp (PropSymbol p) = p
+		ppn (NomSymbol n) = n
+		ppm (RelSymbol m) = m	
+		convWrap = wrapInPar . converter
 
 cASLHeader :: SymSet -> String
 cASLHeader (a,b,c) =
@@ -39,37 +66,50 @@ cASLHeader (a,b,c) =
 	where
 		f = concat . (intersperse ",") . elems
 
-
-
 addCASLHeader :: SymSet -> String -> String
 addCASLHeader x = (cASLHeader x ++) 
 -- the function below collects symbols from a list
 -- of formulas, returning a triple of sets of type
 -- (Set prop, Set noms, Set mods)
 collectSymbols :: InputFile -> SymSet
-collectSymbols = foldr f emptytr
+collectSymbols = foldr f triEmpty
 	where
-		emptytr = (empty, empty, empty) 
 		--pretty print
 		ppp (PropSymbol p) = p
 		ppn (NomSymbol n) = n
 		ppm (RelSymbol m) = m
+		-- inserter
+		addProp (PropSymbol p) (a,b,c) = (insert p a, b,c )
+		addNom (NomSymbol n) (a,b,c) = (a, insert n b, c)
+		addMod (RelSymbol m) (a,b,c) = (a, b, insert m c)
 		--symbol collector
 		f Top s = s
 		f Bot s = s
-		f (Prop p) (a,b,c) = (insert (ppp p) a, b, c)
-		f (Nom n) (a,b,c) = (a, insert (ppn n) b, c)
+		f (Prop p) s = addProp p s
+		f (Nom n) s = addNom n s
 		f (Neg g) s = f g s
-		f (g :&: g') s = uniTrip (f g s) (f g' emptytr)
-		f (g :|: g') s = uniTrip (f g s) (f g' emptytr)
-		f (g :-->: g') s = uniTrip (f g s) (f g' emptytr)
-		f (g :<-->: g') s = uniTrip (f g s) (f g' emptytr)
-		f (Diam r g) (a,b,c) = f g (a,b,insert (ppm r) c)
-		f (Box r g) (a,b,c) = f g (a,b,insert (ppm r) c)
-		f (At n g) (a,b,c) = f g (a, insert (ppn n) b,c)
+		f (g :&: g') s = tripUnion (f g s) (f g' triEmpty)
+		f (g :|: g') s = tripUnion (f g s) (f g' triEmpty)
+		f (g :-->: g') s = tripUnion (f g s) (f g' triEmpty)
+		f (g :<-->: g') s = tripUnion (f g s) (f g' triEmpty)
+		f (Diam r g) s = f g (addMod r s)
+		f (Box r g) s = f g (addMod r s)
+		f (At n g) s = f g (addNom n s)
 		f (A g) s = f g s
 		f (E g) s = f g s
 		f (Down n g) s = f g s
 
-uniTrip :: SymSet -> SymSet -> SymSet
-uniTrip (a,b,c) (a',b',c') = (union a a', union b b', union c c')
+wrap :: String -> String -> String -> String
+wrap w s w'  = w++" "++s++" "++w'
+
+wrapInPar :: String -> String
+wrapInPar s = wrap "(" s ")" 
+
+wrapInDia :: String -> String
+wrapInDia s = wrap "<" s ">"
+
+wrapInBox :: String -> String
+wrapInBox s = wrap "[" s "]"
+
+tripUnion :: SymSet -> SymSet -> SymSet
+tripUnion (a,b,c) (a',b',c') = (union a a', union b b', union c c')
